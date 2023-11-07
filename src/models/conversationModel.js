9d15b69531da502bef0ae4745ca25147f5bbbad1
Rakibhsaniu <rakibhasanoyoniu@gmail.com>
2023-11-07 16:43:49 +0600

    conversationModel created

--- src/models/conversationModel.js
+++ src/models/conversationModel.js
@@ -1,18 +1,18 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const DataSchema = mongoose.Schema(
-  {
-    conversationID: { type: String, unique: true },
-    creatorID: { type: String },
-    creatorRole: { type: String },
-    participantID: { type: String },
-    participantRole: { type: String },
-    lastUpdatedAT: { type: Date },
-    selectedPackageID: { type: String },
-  },
-  { versionKey: false }
-);
+// const DataSchema = mongoose.Schema(
+//   {
+//     conversationID: { type: String, unique: true },
+//     creatorID: { type: String },
+//     creatorRole: { type: String },
+//     participantID: { type: String },
+//     participantRole: { type: String },
+//     lastUpdatedAT: { type: Date },
+//     selectedPackageID: { type: String },
+//   },
+//   { versionKey: false }
+// );
 
-const conversationModel = mongoose.model("conversations", DataSchema);
+// const conversationModel = mongoose.model("conversations", DataSchema);
 
-module.exports = conversationModel;
+// module.exports = conversationModel;

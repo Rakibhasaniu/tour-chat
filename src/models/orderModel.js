a5c7ba4c69ad78423b5f3ced5c96d4070fdb3975
Rakibhsaniu <rakibhasanoyoniu@gmail.com>
2023-11-09 19:02:11 +0600

    order model schema finished

--- src/models/orderModel.js
+++ src/models/orderModel.js
@@ -1,20 +1,20 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// const DataSchema= mongoose.Schema({
-//         orderID:{type:String},
-//         packageID:{type:String},
-//         instructorID:{type:String},
-//         orderDescription:{type:String},
-//         orderCreatedDate:{type:Date},
-//         orderUpdatedDate:{type:Date},
-//         orderStatus:{type:String},
-//         orderPrice:{type:Number},
-//         orderNumber:{type:Number},
-//         activeStatus:{type:String},
-//         clientID:{type:String}
+const DataSchema= mongoose.Schema({
+        orderID:{type:String},
+        packageID:{type:String},
+        instructorID:{type:String},
+        orderDescription:{type:String},
+        orderCreatedDate:{type:Date},
+        orderUpdatedDate:{type:Date},
+        orderStatus:{type:String},
+        orderPrice:{type:Number},
+        orderNumber:{type:Number},
+        activeStatus:{type:String},
+        clientID:{type:String}
 
-// },{versionKey: false});
+},{versionKey: false});
 
-// const orderModel = mongoose.model("orders",DataSchema);
+const orderModel = mongoose.model("orders",DataSchema);
 
-// module.exports = orderModel;
+module.exports = orderModel;

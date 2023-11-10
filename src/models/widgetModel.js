5a7d4e647fa0800b06dfb064f1e56acf0930e9d0
Rakibhsaniu <rakibhasanoyoniu@gmail.com>
2023-11-10 19:19:45 +0600

    widget model schema created

--- src/models/widgetModel.js
+++ src/models/widgetModel.js
@@ -1,19 +1,19 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const DataSchema= mongoose.Schema({
-   widgetName: {
-    type: String, 
-    required: true,
-    unique: true
-    },
-   widgetPayload:[],
-   activeStatus:{
-      type: String,
-      required: true,
-  }
+// const DataSchema= mongoose.Schema({
+//    widgetName: {
+//     type: String, 
+//     required: true,
+//     unique: true
+//     },
+//    widgetPayload:[],
+//    activeStatus:{
+//       type: String,
+//       required: true,
+//   }
 
-},{versionKey: false});
+// },{versionKey: false});
 
-const widgetModel = mongoose.model("widgets",DataSchema);
+// const widgetModel = mongoose.model("widgets",DataSchema);
 
-module.exports = widgetModel;
+// module.exports = widgetModel;

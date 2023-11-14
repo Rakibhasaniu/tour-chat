bf0d38c115d86e3efb16ac10b4b769c3e48a3bcc
Rakibhsaniu <rakibhasanoyoniu@gmail.com>
2023-11-14 22:28:51 +0600

    check login finished

--- apiservices/checklogin.js
+++ apiservices/checklogin.js
@@ -1,79 +1,75 @@
-// const axios = require("axios");
+const axios = require("axios");
 
-// exports.isAdmin = async() => {
+exports.isAdmin = async () => {
+  const response = await axios.get("/apis/v1/isAdmin", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
-//     const response = await axios.get('/apis/v1/isAdmin', {
-//         headers: {
-//             'Content-Type': 'application/json'
-//         }
-//     });
+  if (!response.status === 200) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
 
-//     if (!response.status === 200) {
-//         // This will activate the closest `error.js` Error Boundary
-//         throw new Error('Failed to fetch data');
-//     }
+  return response.data;
+};
 
-//     return response.data;
-// }
+exports.clientLogin = async (userName, password) => {
+  const payloaddata = {
+    userName,
+    password,
+  };
+  const res = await fetch("/apis/v1/client-login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payloaddata),
+  });
 
-// exports.clientLogin = async(userName, password) => {
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
 
-//     const payloaddata ={
-//         userName,
-//         password
-//     };
-//     const res = await fetch('/apis/v1/client-login', {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify(payloaddata),
-//     })
+  return res.json();
+};
 
-//     if(!res.ok) {
-//         // This will activate the closest `error.js` Error Boundary
-//         throw new Error('Failed to fetch data')
-//     }
+exports.instructorLogin = async (userName, password) => {
+  const payloaddata = {
+    userName,
+    password,
+  };
+  const res = await fetch("/apis/v1/instructor-login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payloaddata),
+  });
 
-//     return res.json()
-// }
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
 
-// exports.instructorLogin = async(userName, password) => {
+  return res.json();
+};
 
-//     const payloaddata ={
-//         userName,
-//         password
-//     };
-//     const res = await fetch('/apis/v1/instructor-login', {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify(payloaddata),
-//     })
+exports.logout = async () => {
+  const res = await fetch("/apis/v1/logout", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+  });
 
-//     if(!res.ok) {
-//         // This will activate the closest `error.js` Error Boundary
-//         throw new Error('Failed to fetch data')
-//     }
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
 
-//     return res.json()
-// }
-
-// exports.logout = async() => {
-
-//     const res = await fetch('/apis/v1/logout', {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         credentials: "include"
-//     })
-
-//     if(!res.ok) {
-//         // This will activate the closest `error.js` Error Boundary
-//         throw new Error('Failed to fetch data')
-//     }
-
-//     return res.json()
-// }
+  return res.json();
+};
